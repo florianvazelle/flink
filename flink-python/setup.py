1638c40d2473b2eec4d0c10be2e2bff09987c498
florian <florian@harfanglab.fr>
2026-07-27 15:19:53 +0200

    Upgrade pyarrow to 25.0.0

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -325,7 +325,7 @@ try:
                         'protobuf>=3.19.0',
                         'numpy>=1.22.4',
                         'pandas>=1.3.0,<2.3',  # FLINK-38513: 2.3+ drops cp39 wheels
-                        'pyarrow>=5.0.0,<21.0.0',
+                        'pyarrow>=5.0.0,<26.0.0',
                         'pemja>=0.5.7,<0.5.8;platform_system != "Windows"',
                         'httplib2>=0.19.0',
                         'ruamel.yaml>=0.18.4',
